f2739143442705a02c7916a76c2b9c6a7b5dd32e
Ivar Johnson <ivar@ivardesign.com>
2020-11-08 18:22:42 -0800

    Adds an array of sizes to the data object and uses v-for to display them in a list.

--- main.js
+++ main.js
@@ -4,7 +4,12 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['100% cotton'],
+            variants: [
+              { id: 2234, color: 'Green' },
+              { id: 2235, color: 'Blue' }
+            ],
+            sizes: [ 'Small', 'Medium', 'Large' ]
         }
     }
 })
