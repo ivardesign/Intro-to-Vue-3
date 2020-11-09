296238eefbfcc22e294b29d07c975f512b13081a
Ivar Johnson <ivar@ivardesign.com>
2020-11-08 16:37:50 -0800

    added product and description

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+  data() {
+    return {
+      product: 'Socks',
+      description: 'Green and white stripes, thigh-high, extra thick, 100% cotton.'
+    }
+  }
+})
